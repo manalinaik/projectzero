9dd5f1a90c27f0c6d4eaf213a6360dc317c75320
manali <mnaik@mit.edu>
2013-02-07 14:50:05 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projectzero::Application.config.secret_token = 'fa68e8fb471a97741f97d91caf7bd55120d3f0ccb951ac166c2d09eabe2548d6d944cca4c26c8bd1b34341c738d1133f97b63f69d5e65b50c67fcf2994304d45'
